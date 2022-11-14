bbc503a830c6cd3f731927a95f23978b6b2cf539
Julia Lima <julia.manvailer17@icloud.com>
2022-11-14 15:29:21 -0500

    components and pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Swipe from './pages/Swipe'
+import Posts from './pages/Posts'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/main" element={<Swipe />} />
+          <Route path="/post" element={<Posts />} />
+        </Routes>
+      </main>
+    </div>
+  )
 }
 
 export default App
