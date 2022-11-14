71b1b65af5ff971c207af4879aeb9dfbcdc14823
Julia Lima <julia.manvailer17@icloud.com>
2022-11-14 16:33:15 -0500

    home nav and form

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,18 @@
 import LoginNav from '../components/LoginNav'
+import RegisterForm from '../components/RegisterForm'
+import '../CSS/Home.css'
 
 const Home = () => {
-  return <LoginNav />
+  return (
+    <div className="home">
+      <div className="nav-holder">
+        <LoginNav />
+      </div>
+      <div className="form-holder">
+        <RegisterForm />
+      </div>
+    </div>
+  )
 }
 
 export default Home
