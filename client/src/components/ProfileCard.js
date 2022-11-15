a97965a32f9117673b6d8ada4164f5a930a5fde1
Jen Dunlap <jenniferleighdunlap@gmail.com>
2022-11-15 10:49:11 -0500

    profile card

--- client/src/components/ProfileCard.js
+++ client/src/components/ProfileCard.js
@@ -1,16 +1,40 @@
 const ProfileCard = (props) => {
+  const swipeRight = () => {}
+
+  const swipeLeft = () => {}
+
   return (
     <div className="cardDiv">
-      <div className="imageWrapper">
-        <img
-          className="userAvatar"
-          src={props.avatar}
-          alt={props.userName}
-        ></img>
+      <div className="cardDivFront">
+        <div className="imageWrapper">
+          <img
+            className="userAvatar"
+            src={props.avatar}
+            alt={props.userName}
+          ></img>
+        </div>
+        <div className="infoWrapper">
+          <h1>{props.userName}</h1>
+          <div className="skills">{props.skills.map()}</div>
+        </div>
       </div>
-      <div className="infoWrapper">
-        <h1>{props.userName}</h1>
+      <img src="" alt="">
+        //onClick to pull up cardDivInfo???
+      </img>
+      <div className="cardDivInfo">
+        <div className="userProjects">{props.projects.map()}</div>
+        <div className="userSocial">{props.socialLinks.map()}</div>
+      </div>
+      <div className="swipeButtons">
+        <img src="" alt="">
+          //onClick for swipeRight
+        </img>
+        <img src="" alt="">
+          //onClick for swipeLeft
+        </img>
       </div>
     </div>
   )
 }
+
+export default ProfileCard
--- client/src/pages/Swipe.js
+++ client/src/pages/Swipe.js
@@ -1,4 +1,5 @@
 import Nav from '../components/Nav'
+// import ProfileCard from '../components/ProfileCard'
 import '../CSS/Swipe.css'
 
 const Swipe = () => {
@@ -9,7 +10,15 @@ const Swipe = () => {
       </div>
       <div className="swipe">
         <h1>Profile Card Here</h1>
-        {/* <ProfileCard /> */}
+        {/* <ProfileCard 
+          id={ProfileCard.id}
+          key={ProfileCard.id}
+          image={ProfileCard.avatar}
+          name={ProfileCard.name}
+          username={ProfileCard.username}
+          socialLinks={ProfileCard.socialLinks}
+          projects={ProfileCard.projects}
+        /> */}
       </div>
     </div>
   )
