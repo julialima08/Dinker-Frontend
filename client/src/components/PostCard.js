db7622bad0ff239a3c61fc8aba2cba6d946baa2c
John <jboyle15@yahoo.com>
2022-11-14 19:48:23 -0600

    console logging post data

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,9 +1,25 @@
-const PostCard = () => {
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+import { BASE_URL } from '../globals'
+
+const PostCard = (props) => {
+  const [post, setPost] = useState({})
+  const { id } = useParams()
+
+  const postCardStyle = {
+    border: '3px solid green',
+    width: '150px',
+    height: '120px',
+    display: 'block',
+    padding: '30px 0',
+    margin: 'auto'
+  }
   return (
-    <div className="PostCard">
-      <div>Title</div>
-      <div>Body</div>
-      <div>Skills</div>
+    <div className="PostCard" style={postCardStyle}>
+      {/* <h1>{`${props.post.title}`}</h1>
+      <p>{`${props.post.body}`}</p>
+      <p>{`${props.post.skills}`}</p> */}
     </div>
   )
 }
--- client/src/pages/Posts.js
+++ client/src/pages/Posts.js
@@ -1,6 +1,19 @@
 import PostCard from '../components/PostCard'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { BASE_URL } from '../globals'
 
 const Posts = (props) => {
+  const [Posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const res = await axios.get(`${BASE_URL}/api/posts`)
+      console.log(res.data)
+      setPosts(res.data)
+    }
+    getPosts()
+  }, [])
   return (
     <div className="Posts">
       <h1>Posts</h1>
