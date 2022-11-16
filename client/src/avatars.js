0a063409a291b09b728710a4469c380051d3af13
Jen Dunlap <jenniferleighdunlap@gmail.com>
2022-11-16 17:54:49 -0500

    form and styling changes

---
+++ client/src/avatars.js
@@ -0,0 +1,15 @@
+const avatars = [
+  { name: '1', url: 'https://i.imgur.com/aWrwP5x.png' },
+  { name: '2', url: 'https://i.imgur.com/7RvwMJZ.png' },
+  { name: '3', url: 'https://i.imgur.com/MBF6BjS.png' },
+  { name: '4', url: 'https://i.imgur.com/k46gwRb.png' },
+  { name: '5', url: 'https://i.imgur.com/0NRiRWf.png' },
+  { name: '6', url: 'https://i.imgur.com/ESXbqHx.png' },
+  { name: '7', url: 'https://i.imgur.com/8WQ3TOd.png' },
+  { name: '8', url: 'https://i.imgur.com/4t9AAw6.png' },
+  { name: '9', url: 'https://i.imgur.com/0vn5IvM.png' },
+  { name: '10', url: 'https://i.imgur.com/4ldHOSn.png' },
+  { name: '11', url: 'https://i.imgur.com/Gvt3akV.png' }
+]
+
+export default avatars
