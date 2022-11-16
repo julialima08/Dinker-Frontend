7ec04b0532a09178b367e9848fca7849ed2eb16c
Julia Lima <julia.manvailer17@icloud.com>
2022-11-16 17:53:10 -0500

    working buttons

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -8,6 +8,7 @@ import SwipeCard from '../animation code/swipeAnimation'
 
 const Main = () => {
   const [users, setUsers] = useState([])
+  let userId = localStorage.getItem('id')
 
   const getUsers = async () => {
     const response = await axios.get(`${BASE_URL}/api/users`)
@@ -18,11 +19,36 @@ const Main = () => {
     getUsers()
   }, [])
 
+  const [matchId, setMatchId] = useState({ matchId: null })
+  const swipeRight = async (id, idx) => {
+    setMatchId({ matchId: id })
+    console.log(matchId)
+    if (matchId != null) {
+      const makeMatch = await axios.post(
+        `${BASE_URL}/api/users/match/${userId}`,
+        matchId
+      )
+      console.log(makeMatch)
+    } else {
+      // setMatchId({ matchId: id })
+    }
+  }
+
   //credit for swipe animation: github- 3DJakob
 
   const [currentIndex, setCurrentIndex] = useState(users.length - 1)
   const [lastDirection, setLastDirection] = useState()
 
+  const outOfFrame = (id, idx) => {
+    console.log(`${id} (${idx}) left the screen!`, currentIndexRef.current)
+    // handle the case in which go back is pressed before card goes outOfFrame
+    currentIndexRef.current >= idx && childRefs[idx].current.restoreCard()
+    // const res = await axios.get(`${BASE_URL}/api/users/${idx}`)
+    // const id = parseInt(Object.values(idx))
+    // console.log(idx)
+    // setMatchId({ matchId: id })
+    // swipeRight()
+  }
   const currentIndexRef = useRef(currentIndex)
 
   const childRefs = useMemo(
@@ -48,12 +74,6 @@ const Main = () => {
     updateCurrentIndex(index - 1)
   }
 
-  const outOfFrame = (id, idx) => {
-    console.log(`${id} (${idx}) left the screen!`, currentIndexRef.current)
-    // handle the case in which go back is pressed before card goes outOfFrame
-    currentIndexRef.current >= idx && childRefs[idx].current.restoreCard()
-  }
-
   const swipe = async (dir) => {
     if (canSwipe && currentIndex < users.length) {
       await childRefs[currentIndex].current.swipe(dir) // Swipe the card!
@@ -77,37 +97,27 @@ const Main = () => {
       </div>
       <div className="swipePg">
         <div className="swipeGrid">
-          {users.map((user, index) => (
-            <SwipeCard
-              ref={childRefs[index]}
-              className="swipe"
-              key={user.id}
-              onSwipe={(dir) => swiped(dir, user.id, index)}
-              onCardLeftScreen={() => outOfFrame(user.id, index)}
-            >
-              <div className="swipeCard">
-                <ProfileCard user={user} key={user.id} />
-              </div>
-            </SwipeCard>
-          ))}
+          {users
+            .sort((a, b) => a.id - b.id)
+            .map((user, index) => (
+              <SwipeCard
+                ref={childRefs[index]}
+                className="swipe"
+                key={user.id}
+                onSwipe={(dir) => swiped(dir, user.id, index)}
+                onCardLeftScreen={() => outOfFrame(user.id, index)}
+              >
+                <div className="swipeCard">
+                  <ProfileCard
+                    user={user}
+                    key={user.id}
+                    swipeRight={() => swipeRight(user.id, index)}
+                  />
+                </div>
+              </SwipeCard>
+            ))}
         </div>
-        <div className="buttons">
-          <img
-            className="swipeLeft"
-            src="https://i.imgur.com/TwVQBFx.png"
-            alt=""
-          />
-          <img
-            className="moreInfo"
-            src="https://i.imgur.com/71LLr11.png"
-            alt=""
-          ></img>
-          <img
-            className="swipeRight"
-            src="https://i.imgur.com/OgR2xvL.png"
-            alt=""
-          />
-        </div>
+        <div className="buttons"></div>
       </div>
     </div>
   )
