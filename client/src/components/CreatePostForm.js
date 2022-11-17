f7a51eab99ad1bc8ee6b562b23fd8f05581dab04
John <jboyle15@yahoo.com>
2022-11-16 18:00:51 -0600

    refresh on submit still not working

--- client/src/components/CreatePostForm.js
+++ client/src/components/CreatePostForm.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react'
 // import { useNavigate } from 'react-router-dom'
 let userId = localStorage.getItem('id')
 
-const CreatePostForm = () => {
+const CreatePostForm = (props) => {
   const [userInfo, setUserInfo] = useState(null)
   const [userAvatar, setUserAvatar] = useState(null)
 
@@ -16,13 +16,15 @@ const CreatePostForm = () => {
   useEffect(() => {
     getUserInfo()
   }, [])
-
-  // console.log(userInfo)
+  // useEffect(() => {
+  //   props.getPosts()
+  // }, [])
+  console.log(props.getPosts)
 
   const initialState = {
     title: '',
     body: '',
-    skills: ''
+    skills: []
   }
   const [formState, setFormState] = useState(initialState)
   const handleSubmit = async (event) => {
@@ -60,15 +62,32 @@ const CreatePostForm = () => {
           // required
           onChange={handleChange}
         />
-        <input
-          id="skills"
-          value={formState.skills}
-          placeholder="Skills you are looking for"
-          // required
-          onChange={handleChange}
-        />
+        <div className="formDiv">
+          <label htmlFor="skills" className="formLabel">
+            PRIMARY SKILL
+          </label>
+          <select
+            onChange={handleChange}
+            type="checkbox"
+            id="skills"
+            value={formState.skills}
+          >
+            <option value="HTML">HTML</option>
+            <option value="CSS">CSSL</option>
+            <option value="JavaScript">JavaScript</option>
+            <option value="React">React</option>
+            <option value="MongoDB">MongoDB</option>
+            <option value="Express">Express</option>
+            <option value="PostgreSQL">PostgreSQL</option>
+            <option value="Sequelize">Sequelize</option>
+            <option value="Auth">Auth</option>
+            <option value="Vue">Vue.jsL</option>
+            <option value="Python">Python</option>
+            <option value="Flask">Flask</option>
+          </select>
+        </div>
         <button type="submit">Submit New Post!</button>
-        {/* <button onClick={() => navigate('/post')}>Return to posts list</button> */}
+        {/* <button type="submit">Submit New Post!</button> */}
       </form>
     </div>
   )
